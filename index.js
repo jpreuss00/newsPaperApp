c5d4a36316d7e529a54d701d2ee643dfbf56cc27
Preuß <joshua.preuss2000@gmx.de>
2020-01-17 12:45:56 +0100

    working search with api

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ $(document).ready(function () {
     $(".loginSection").show();
     $(".searchSection").hide();
     $(".searchSection__articles").hide();
+    $(".searchSection__articles--invalid").hide();
+    $(".searchSection__main--invalid").hide();
 
     // register event handlers
     $(".login__confirmButton").click(handleLogin);
@@ -11,51 +13,122 @@ $(document).ready(function () {
 
 });
 
-function checkLogin(username, password) { 
-    return (username == 123 && password == 123);
-}
-
 function handleLogin() {
     const username = $('.login__username').val();
     const password = $('.login__password').val();
-
-    if (checkLogin(username, password)) {
-        $(".login").removeClass("login--invalid");
-        $(".login__username").removeClass('login__username--invalid');
-        $(".login__password").removeClass('login__password--invalid');
-        $(".login__confirmButton").removeClass('login__confirmButton--invalid');
-        $("header").removeClass('header--invalid');
-        $("footer").removeClass('footer--invalid');
-        $(".login").fadeOut(function () {
-            $(".searchSection").show();
-        });
-    } else {
-        $(".login").addClass('login--invalid');
-        $("header").addClass('header--invalid');
-        $("footer").addClass('footer--invalid');
-        $(".login__username").addClass('login__username--invalid');
-        $(".login__password").addClass('login__password--invalid');
-        $(".login__confirmButton").addClass('login__confirmButton--invalid');
-        $(".login__username").val('');
-        $(".login__password").val('');
-    }
+    $.get({
+        url: "https://sandbox-api.ipool.asideas.de/sandbox/api/search?q=test",
+        success: function () {
+            $(".login").removeClass("login--invalid");
+            $(".login__username").removeClass('login__username--invalid');
+            $(".login__password").removeClass('login__password--invalid');
+            $(".login__confirmButton").removeClass('login__confirmButton--invalid');
+            $(".header").removeClass('header--invalid');
+            $(".footer").removeClass('footer--invalid');
+            $(".login").fadeOut(function () {
+                $(".searchSection").show();
+            });
+        },
+        statusCode: {
+            401: function () {
+                $(".login").addClass('login--invalid');
+                $(".header").addClass('header--invalid');
+                $(".footer").addClass('footer--invalid');
+                $(".login__username").addClass('login__username--invalid');
+                $(".login__password").addClass('login__password--invalid');
+                $(".login__confirmButton").addClass('login__confirmButton--invalid');
+                $(".login__username").val('');
+                $(".login__password").val('');
+            }
+        },
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password));
+        }
+    });
 }
 
-var randomtext = "In show dull give need so held. One order all scale sense her gay style wrote. Incommode our not one ourselves residence. Shall there whose those stand she end. So unaffected partiality indulgence dispatched to of celebrated remarkably. Unfeeling are had allowance own perceived abilities. Admiration stimulated cultivated reasonable be projection possession of. Real no near room ye bred sake if some. Is arranging furnished knowledge agreeable so. Fanny as smile up small. It vulgar chatty simple months turned oh at change of. Astonished set expression solicitude way admiration. Mind what no by kept. Celebrated no he decisively thoroughly. Our asked sex point her she seems. New plenty she horses parish design you. Stuff sight equal of my woody. Him children bringing goodness suitable she entirely put far daughter.";
-
 function handleSearch() {
-    $("<article>" + "<p>" + randomtext + "</p>" + "</article>").appendTo(".searchSection").attr({
-        class: "searchSection__articles"
+    var searchLimit = $('.search__limit').val();
+    const searchWords = $('.search__bar').val();
+    const username = $('.login__username').val();
+    const password = $('.login__password').val();
+
+    $(".searchSection__articles").remove();
+    $(".searchSection__articles--invalid").hide();
+    $(".searchSection__main--invalid").hide();
+
+    $.get({
+        url: "https://sandbox-api.ipool.asideas.de/sandbox/api/search?q=" + encodeURI(searchWords) + "&limit=" + searchLimit,
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password));
+        },
+        success: function (data) {
+            if (data.pagination.total != 0) {
+                if (searchLimit > 99) {
+                    searchLimit = 99
+                }
+                for (i = 0; i < searchLimit; i++) {
+                    $("<article>" + "<header>" + "<H3>" + data.documents[i].title + "</H3>" + "</header>" + "<main>" + data.documents[i].content + "</main>" + "<footer>" + "<a href =" + data.documents[i].url + ">Go to the Website to read the full article" + "</a>" + "</footer>" + "</article>").appendTo(".searchSection").attr({
+                        class: "searchSection__articles"
+                    })
+                    $("header").attr({
+                        class: "searchSection__header"
+                    })
+                    $("main").attr({
+                        class: "searchSection__main"
+                    })
+                    $("footer").attr({
+                        class: "searchSection__footer"
+                    })
+                }
+                searchIndicator();
+            } else {
+                errorMessage();
+            }
+        },
+        statusCode: {
+            400: function () {
+                errorMessage();
+            },
+            401: function () {
+                errorMessage();
+            },
+            500: function () {
+                errorMessage();
+            }
+        }
     });
+}
+
+function errorMessage() {
+    $(".search__bar").addClass('search__bar--invalid');
+    $(".search__button").addClass('search__button--invalid');
+    $(".search__limit").addClass('search__limit--invalid');
+    $(".searchSection__articles--invalid").show();
+    $(".searchSection__main--invalid").show();
+    $(".header").addClass('header--invalid');
+    $(".footer").addClass('footer--invalid');
+    setTimeout(function () {
+        $(".search__bar").removeClass('search__bar--invalid');
+        $(".search__button").removeClass('search__button--invalid');
+        $(".search__limit").removeClass('search__limit--invalid');
+        $(".header").removeClass('header--invalid');
+        $(".footer").removeClass('footer--invalid');
+        $(".search__bar").val('');
+    }, 1000);
+
+}
+
+function searchIndicator() {
     $(".search__button").addClass('search__button--active');
     $(".search__limit").addClass('search__limit--active');
-    $("header").addClass('header--active');
-    $("footer").addClass('footer--active');
+    $(".header").addClass('header--active');
+    $(".footer").addClass('footer--active');
     setTimeout(function () {
         $(".search__button").removeClass('search__button--active');
         $(".search__limit").removeClass('search__limit--active');
-        $("header").removeClass('header--active');
-        $("footer").removeClass('footer--active');
+        $(".header").removeClass('header--active');
+        $(".footer").removeClass('footer--active');
         $(".search__bar").val('');
     }, 1000);
 }
