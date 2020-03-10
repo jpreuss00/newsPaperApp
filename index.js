0f72efa9b6902e1a44a3394fb4c2955a8d802165
Preuß <joshua.preuss2000@gmx.de>
2020-03-10 14:11:07 +0100

    update dist

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ function handleLogin() {
     const username = $('.login__username').val();
     const password = $('.login__password').val();
     $.get({
-        url: "https://thawing-ridge-63424.herokuapp.com/health",
+        url: "http://thawing-ridge-63424.herokuapp.com/health",
         success: function () {
             $(".login").removeClass('login--invalid');
             $(".login__username").removeClass('login__username--invalid');
